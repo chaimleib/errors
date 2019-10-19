b4317ab2f5948daf11b4afd63e8d610841160640
Chaim Halbert <chaim.leib.halbert@gmail.com>
2019-10-18 17:27:07 -0700

    tests for wrapped Is support; TODO: tests for As

--- errors_test.go
+++ errors_test.go
@@ -40,3 +40,47 @@ func TestGroup(t *testing.T) {
 	bc := Wrap(fmt.Errorf("c"), "b")
 	assert.Equal(t, "[\n\ta\n\t,\n\tb\n\tc\n]", Group([]error{a, bc}).Error())
 }
+
+func TestWrapIs(t *testing.T) {
+	tokenErr := fmt.Errorf("token")
+	a := Wrap(tokenErr, "a")
+	b := Wrap(tokenErr, "b")
+	unrelated := fmt.Errorf("unrelated")
+
+	assert.True(t, Is(tokenErr, tokenErr))
+	assert.True(t, Is(a, tokenErr))
+	assert.True(t, Is(b, tokenErr))
+	assert.False(t, Is(a, b))
+	assert.False(t, Is(b, a))
+	assert.False(t, Is(unrelated, tokenErr))
+	assert.False(t, Is(a, unrelated))
+	assert.False(t, Is(b, unrelated))
+}
+
+func TestWrapWithIs(t *testing.T) {
+	tokenErr := fmt.Errorf("token")
+	a := fmt.Errorf("a")
+	a1 := WrapWith(tokenErr, a)
+	a2 := WrapWith(tokenErr, a)
+	a3 := Wrap(tokenErr, "a")
+	unrelated := fmt.Errorf("unrelated")
+
+	assert.True(t, Is(a1, a))
+	assert.True(t, Is(a2, a))
+	assert.False(t, Is(a3, a))
+	assert.True(t, Is(a1, a2))
+	assert.True(t, Is(a2, a1))
+	assert.False(t, Is(a1, a3))
+	assert.False(t, Is(a3, a1))
+	assert.False(t, Is(a2, a3))
+	assert.False(t, Is(a3, a2))
+	assert.False(t, Is(a1, unrelated))
+	assert.False(t, Is(a2, unrelated))
+	assert.False(t, Is(a3, unrelated))
+	assert.True(t, Is(a1, tokenErr))
+	assert.True(t, Is(a2, tokenErr))
+	assert.True(t, Is(a3, tokenErr))
+	assert.False(t, Is(tokenErr, a1))
+	assert.False(t, Is(tokenErr, a2))
+	assert.False(t, Is(tokenErr, a3))
+}
